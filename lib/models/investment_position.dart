8d50dde36926dbf93080e6aa5ef1e6a7e6be623b
LucasPerso <lucas.herfort@gmail.com>
2026-01-12 18:49:12 +0100

    change

--- lib/models/investment_position.dart
+++ lib/models/investment_position.dart
@@ -8,6 +8,12 @@ class InvestmentPosition {
   final double pru;
   double? currentPrice;
 
+  // Nouvelles infos depuis Google Sheet
+  String? currency;
+  double? priceOpen;
+  double? high;
+  double? low;
+  int? volume;
 
   InvestmentPosition({
     required this.id,
@@ -17,7 +23,12 @@ class InvestmentPosition {
     required this.supportType,
     required this.quantity,
     required this.pru,
-    this.currentPrice
+    this.currentPrice,
+    this.currency,
+    this.priceOpen,
+    this.high,
+    this.low,
+    this.volume,
   });
 
   // Valeur totale de la position
@@ -30,14 +41,31 @@ class InvestmentPosition {
   double get performance =>
       ((totalValue / (pru * quantity)) - 1) * 100;
 
-
   // Méthode pour mettre à jour avec les données du Google Sheet
   void updateFromSheet(Map<String, dynamic> sheetData) {
     name = sheetData['name']?.toString() ?? name;
+    currency = sheetData['currency']?.toString();
 
     if (sheetData['price'] != null) {
       final priceValue = sheetData['price'].toString();
       currentPrice = double.tryParse(priceValue.replaceAll(',', '.').replaceAll(' ', ''));
+    }
+
+    if (sheetData['priceopen'] != null) {
+      final value = sheetData['priceopen'].toString();
+      priceOpen = double.tryParse(value.replaceAll(',', '.').replaceAll(' ', ''));
+    }
+    if (sheetData['high'] != null) {
+      final value = sheetData['high'].toString();
+      high = double.tryParse(value.replaceAll(',', '.').replaceAll(' ', ''));
+    }
+    if (sheetData['low'] != null) {
+      final value = sheetData['low'].toString();
+      low = double.tryParse(value.replaceAll(',', '.').replaceAll(' ', ''));
+    }
+    if (sheetData['volume'] != null) {
+      final value = sheetData['volume'].toString();
+      volume = int.tryParse(value.replaceAll(',', '').replaceAll(' ', ''));
     }
   }
 }
